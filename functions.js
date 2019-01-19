106bf8562421ce961f45dfc5e479d1cb2375c620
Zainab <33379225+PineapplePatch@users.noreply.github.com>
2019-01-19 11:23:59 -0800

    Create functions.js

---
+++ functions.js
@@ -0,0 +1,13 @@
+var count = 0
+
+function like() {
+  count += 1;
+  document.getElementById('count').innerHTML = count;
+}
+
+var count = 0
+
+function dislike() {
+  count -= 1;
+  document.getElementById('count').innerHTML = count;
+}
