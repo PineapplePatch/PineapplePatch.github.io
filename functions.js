ff591ec09c50f3efbaa5b4ef6c96be249ff21c53
Zainab <33379225+PineapplePatch@users.noreply.github.com>
2019-02-16 10:49:19 -0800

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,11 +1,1 @@
-var count = 0
 
-function like() {
-  count += 1;
-  document.getElementById('count').innerHTML = count;
-}
-
-function dislike() {
-  count -= 1;
-  document.getElementById('count').innerHTML = count;
-}
